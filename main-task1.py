c018a1e7b6621d8ea4a1d921e0f92cc9da663623
Valentyna Feshchenko <fesval777@gmail.com>
2022-12-20 15:01:24 +0100

    Init project

---
+++ main-task1.py
@@ -0,0 +1,14 @@
+my_dict = {
+    "пекарня": ["хлеб", "булочки", "пончик"],
+    "овощной": ["морковь", "петрушка", "рукола"]
+    }
+  
+my_dict={key.capitalize():[x.capitalize() for x in value] 
+  for key,value in my_dict.items()}
+
+for key in my_dict.keys():
+  print("Иду в ", key, ",","покупаю тут такие товары:", my_dict[key], ".")
+
+ 
+num=sum(len(value) for value in my_dict.values()) 
+print("Всего покупаю",num, "товаров")

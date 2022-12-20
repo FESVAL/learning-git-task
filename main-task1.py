701f5e2e07749f3ec448c3147c5a086fed9b3b6f
Valentyna Feshchenko <fesval777@gmail.com>
2022-12-20 18:59:16 +0100

    count len(value) and print sum

--- main-task1.py
+++ main-task1.py
@@ -9,3 +9,5 @@ my_dict={key.capitalize():[x.capitalize() for x in value]
 for key in my_dict.keys():
   print("Иду в ", key, ",","покупаю тут такие товары:", my_dict[key], ".")
 
+num=sum(len(value) for value in my_dict.values()) 
+print("Всего покупаю",num, "товаров")

2c100eef3d5990df6d8663452b05a39c0cc7ed17
Valentyna Feshchenko <fesval777@gmail.com>
2022-12-20 15:53:54 +0100

    Init project2 - capitalize my dict

--- main-task1.py
+++ main-task1.py
@@ -3,12 +3,4 @@ my_dict = {
     "овощной": ["морковь", "петрушка", "рукола"]
     }
   
-my_dict={key.capitalize():[x.capitalize() for x in value] 
-  for key,value in my_dict.items()}
 
-for key in my_dict.keys():
-  print("Иду в ", key, ",","покупаю тут такие товары:", my_dict[key], ".")
-
- 
-num=sum(len(value) for value in my_dict.values()) 
-print("Всего покупаю",num, "товаров")

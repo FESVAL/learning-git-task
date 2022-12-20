3afe8055877ec6ee48ece05b66d1b79e7c183e8c
Valentyna Feshchenko <fesval777@gmail.com>
2022-12-20 18:56:58 +0100

    print text, keys, values

--- main-task1.py
+++ main-task1.py
@@ -5,3 +5,7 @@ my_dict = {
 
 my_dict={key.capitalize():[x.capitalize() for x in value] 
   for key,value in my_dict.items()}
+  
+for key in my_dict.keys():
+  print("Иду в ", key, ",","покупаю тут такие товары:", my_dict[key], ".")
+

9e78e259845ca622e9d3a86abf69e2a80d883235
Valentyna Feshchenko <fesval777@gmail.com>
2022-12-20 18:38:39 +0100

    capitalaize my_dict

--- main-task1.py
+++ main-task1.py
@@ -2,5 +2,7 @@ my_dict = {
     "пекарня": ["хлеб", "булочки", "пончик"],
     "овощной": ["морковь", "петрушка", "рукола"]
     }
+my_dict={key.capitalize():[x.capitalize() for x in value] 
+  for key,value in my_dict.items()}
 
 
